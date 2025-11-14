e20473869cf3b26e23afb5119a766f5539c05e52
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-14 09:47:43 -0500

    Update BoohbahMessageDecryptor.java

--- ThreatAwakens/BoohbahMessageDecryptor.java
+++ ThreatAwakens/BoohbahMessageDecryptor.java
@@ -83,6 +83,7 @@ public class BoohbahMessageDecryptor {
         // TODO: Display results to console
             System.out.println("---------------------\n|DECRYPTION COMPLETE|\n---------------------");
             System.out.println("DECRYPTED MESSAGE:\n" + decryptedMessage);
+            System.out.println("\nTHREAT IDENTIFIED:\nTHE TELETUBBIES");
 
          } catch (IOException e) {
         // TODO: Handle the IOException appropriately
